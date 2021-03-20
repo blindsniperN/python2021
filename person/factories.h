d65f7c4cb46ad6f0510027daf34aec25e2e3c96c
DimaSavichev <kubamba2002@gmail.com>
2021-03-20 13:50:09 +0300

    changed factory because of the new database idea (that will be done later)

--- person/factories.h
+++ person/factories.h
@@ -6,24 +6,14 @@
 
 class Factory {
 public:
-    Factory(const std::string& name,
-                    const stats_library::ParameterList& parameters,
-                    const stats_library::SkillList& skills,
-                    const std::vector<int>& att = std::vector<int> (6, 1),
-                    const std::vector<int>& def = std::vector<int> (2, 1),
-                    const std::vector<int>& tool = std::vector<int> (4, 1)):
-                    name_(name), parameters_(parameters), skills_(skills), attack_(att), defense_(def), tools_(tool) {}
-    Person createPerson() const {
-        return Person(name_, parameters_, skills_, attack_, defense_, tools_);
+    pers_class::Person createPerson(const std::string& name,
+    const stats_library::ParameterList& parameters,
+    const stats_library::SkillList& skills,
+    const std::vector<int>& attack = std::vector<int> (6, 1),
+    const std::vector<int>& defense = std::vector<int> (2, 1),
+    const std::vector<int>& tools = std::vector<int> (4, 1)) const {
+        return pers_class::Person(name, parameters, skills, attack, defense, tools);
     };
-
-protected:
-    std::string name_;
-    stats_library::ParameterList parameters_;
-    stats_library::SkillList skills_;
-    std::vector<int> attack_;
-    std::vector<int> defense_;
-    std::vector<int> tools_;
 };
 
 
