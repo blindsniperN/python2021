4ca84dedbb993fa85aed9fb3bd146121af894b43
DimaSavichev <kubamba2002@gmail.com>
2021-04-08 17:17:00 +0300

    database class

---
+++ database/filedatabase/filedatabase.cpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "filedatabase.h"
+#include <fstream>
+
+template <typename T>
+FileDataBase<T>::FileDataBase(const std::string& name): IDataBase<T>(name) {
+    std::ifstream f(name);
+    std::string line;
+    while (f >> line) {
+        T tmp = line;
+        items.emplace(tmp.getName(), tmp);
+    }
+    f.close();
+}
+
+template<typename T>
+void FileDataBase<T>::add(const T& item) {
+    items.emplace(item.getName(), item);
+}
+
+template <typename T>
+void FileDataBase<T>::del(const T& item) {
+    items.erase(item.getName());
+}
+
+template <typename T>
+T FileDataBase<T>::get(const std::string& name) {
+    return items[name];
+}
+
+template <typename T>
+FileDataBase<T>::~FileDataBase() {
+    std::ofstream f(this->name_);
+    for (const auto& [key, value]: items) {
+        f << value.toString() << '\n';
+    }
+    f.close();
+}
---
+++ database/filedatabase/filedatabase.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "../database.h"
+#include <string>
+#include <map>
+using namespace database;
+
+template <typename T>
+class FileDataBase: public IDataBase<T> {
+public:
+    FileDataBase(const std::string&);
+    void add(const T&) override;
+    void del(const T&) override;
+    T get(const std::string&) override;
+    ~FileDataBase() override;
+private:
+    std::map<std::string, T> items;
+};
