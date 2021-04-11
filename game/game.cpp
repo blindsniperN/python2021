32c48eb373c806f72b29566f0adc3ad764dff64e
blindsniperN <spitsyn.na@phystech.edu>
2021-04-11 21:14:37 +0300

    started work on text

--- game/game.cpp
+++ game/game.cpp
@@ -1,34 +1,67 @@
 #include "game.h"
 #include <iostream>
+#include <windows.h>
+#include <string>
+
+Game::Game() {
+  // считываем всё из базы данных
+  std::cout << "Hi, bitches! Do you want to start the game, or do you need some help?\n";
+  Sleep(200);
+  std::cout << "Oh";
+  for (int i = 0; i < 3; ++i) {
+    Sleep(200);
+    std::cout << ".";
+  }
+  std::cout << '\n';
+  Sleep(200);
+  std::cout << "So I cannot hear you...";
+  Sleep(200);
+  std::cout << "Then";
+  for (int i = 0; i < 3; ++i) {
+    Sleep(200);
+    std::cout << ".";
+  }
+  std::cout << '\n';
+  Sleep(200);
+  std::string s = "";
+  while ((s != "START") and (s != "HELP")) {
+    std::cout << "Type START to start game and type HELP to get help!\n";
+    std::cin >> s;
+    if (s == "START") { }
+    else if (s == "HELP") { }
+    else std::cout << "Are you stupid or something? Let's try one more fucking time!\n";
+  }
+}
+
 void Game::ListLevels() {
-    for (Level level: levels_) {
-        std::cout << level << '\n';
-    }
+  for (Level level: levels_) {
+    std::cout << level << '\n';
+  }
 }
 
 void Game::ListTeams() {
-    for (Team team: teams_) {
-        std::cout << team << '\n';
-    }
+  for (Team team: teams_) {
+    std::cout << team << '\n';
+  }
 }
 
 void Game::DeleteLevel(const std::string& name) {
-    auto it = levels_.find(name);
-    if (it != levels_.end()) {
-        levels_.erase(it);
-    } else {
-        std::exception NotFound;
-        throw NotFound;
-    }
+  auto it = levels_.find(name);
+  if (it != levels_.end()) {
+    levels_.erase(it);
+  } else {
+    std::exception NotFound;
+    throw NotFound;
+  }
 }
 
 void Game::DeleteTeam(const std::string& name) {
-    auto it = teams_.find(name);
-    if (it != teams_.end()) {
-        teams_.erase(it);
-    } else {
-        std::exception NotFound;
-        throw NotFound;
-    }
+  auto it = teams_.find(name);
+  if (it != teams_.end()) {
+    teams_.erase(it);
+  } else {
+    std::exception NotFound;
+    throw NotFound;
+  }
 }
 
--- game/game.h
+++ game/game.h
@@ -2,40 +2,40 @@
 #include <string>
 #include <map>
 class Game {
-public:
-    // основные комманды
-    Game(); // подготовка настроек, запуск самой игры
-    void Help();
-    void Play(Level, Team);
+ public:
+  // основные комманды
+  Game(); // подготовка настроек, запуск самой игры
+  ~Game(); // обновление базы данных и корректное завершение игры
+  void Help();
+  void Play(Level, Team);
 
-    // вывод данных
-    void ListPeople();
-    void ListLevels();
-    void ListTeams();
+  // вывод данных
+  void ListPeople();
+  void ListLevels();
+  void ListTeams();
 
-    // создание
-    void CreatePerson();
-    void CreateTeam();
-    void CreateLevel();
+  // создание
+  void CreatePerson();
+  void CreateTeam();
+  void CreateLevel();
 
-    // удаление
-    void DeletePerson(const std::string&);
-    void DeleteTeam(const std::string&);
-    void DeleteLevel(const std::string&);
+  // удаление
+  void DeletePerson(const std::string&);
+  void DeleteTeam(const std::string&);
+  void DeleteLevel(const std::string&);
 
-    // язык
-    std::string Translate(const std::string&); // перевод с русского на английский
-    void ChangeLanguage(const std::string&); // изменить язык игры
+  // язык
+  std::string Translate(const std::string&); // перевод с русского на английский
+  void ChangeLanguage(const std::string&); // изменить язык игры
 
-    // магазин
-    void ListObjects();
-    void BuyObject(const std::string&);
-    int GetBalance();
+  // магазин
+  void ListObjects();
+  void BuyObject(const std::string&);
+  int GetBalance();
 
-private:
-    int state; // что сейчас происходит (нужно для вывода помощи)
-    std::map<std::string, Factory> people_templates_;
-    std::map<std::string, Levels> levels_;
-    std::map<std::string, Team> teams_;
-
+ private:
+  int state; // что сейчас происходит (нужно для вывода помощи)
+  std::map<std::string, Factory> people_templates_;
+  std::map<std::string, Levels> levels_;
+  std::map<std::string, Team> teams_;
 };
